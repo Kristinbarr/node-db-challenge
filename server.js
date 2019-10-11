c3656f570af320857e8b916d49700c1842ed660b
Kristin Barr <kristinbarr@gmail.com>
2019-10-11 13:30:58 -0400

    added helmet, knex cleaner

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const helmet = require('helmet')
 
 const server = express()
 
@@ -6,7 +7,9 @@ const resourceRouter = require('./api/resources/resourceRouter')
 const projectRouter = require('./api/projects/projectRouter')
 // const taskRouter = require('./api/tasks/taskRouter')
 
+server.use(helmet())
 server.use(express.json())
+
 server.use('/api/resources', resourceRouter)
 server.use('/api/projects', projectRouter)
 // server.use('/api/tasks', taskRouter)
