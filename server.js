f1e3f6d43cdf88b858c6e4ceef6b25e7b1c3cd1a
Kristin Barr <kristinbarr@gmail.com>
2019-10-11 12:17:41 -0400

    set up server, index, dbConfig

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const server = express()
+
+const resourceRouter = require('./api/resources/resourceRouter')
+const projectRouter = require('./api/projects/projectRouter')
+// const taskRouter = require('./api/tasks/taskRouter')
+
+server.use(express.json())
+server.use('/api/resources', resourceRouter)
+server.use('/api/projects', projectRouter)
+// server.use('/api/tasks', taskRouter)
+
+server.get('/', (req, res) => {
+  res.send('<h3>test route working</h3>')
+})
+
+module.exports = server
